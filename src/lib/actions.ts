b85c9b29e51d35df705b1229ba619e8258bd3951
Lipo <lipo6942@gmail.com>
2025-06-29 00:01:37 +0000

    au moin change l'image en une voiture pas un tableau d'art

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -47,7 +47,7 @@ export async function createVehicle(formData: FormData) {
     };
   }
   
-  let imageUrl = 'https://images.unsplash.com/photo-1683729748363-4622bd9f1e3a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxMXx8cGV1Z2VvdHxlbnwwfHx8fDE3NTExNTUwOTd8MA&ixlib=rb-4.1.0&q=80&w=1080';
+  let imageUrl = 'https://images.unsplash.com/photo-1503376780353-7e6692767b70?q=80&w=1080';
   let imagePath = '';
 
   try {
