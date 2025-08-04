1ecf19fdce70f01f0376eaf44f83b73c66cf0484
Lipo <lipo6942@gmail.com>
2025-08-04 17:47:07 +0000

    cette commande demande que je migre vers le plan payant , moi je veux ut

---
+++ src/functions/index.ts
@@ -0,0 +1,3 @@
+// This file is intentionally left blank.
+// The automated notification logic has been removed to keep the project on the Firebase free plan.
+// Notifications can be sent manually from the Firebase Cloud Messaging console.
