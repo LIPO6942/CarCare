4753ad10e864c2c6f789246c9fc05a8dbbf4d6bb
Lipo <lipo6942@gmail.com>
2025-12-19 12:23:03 +0000

    le splash screen est toujours la , peut on le retirer et agrder que celu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
           rel="stylesheet"
         ></link>
       </head>
-      <body className="font-body antialiased">
+      <body className="font-body antialiased bg-white">
         <AuthProvider>
             {children}
             <Toaster />
